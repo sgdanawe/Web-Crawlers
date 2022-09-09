2c6a15bec731854a340a785074f8f14502bcd0c2
priyank <priyankmundra61>
2022-09-09 13:53:42 +0530

    remove f strings

--- instagram.py
+++ instagram.py
@@ -31,7 +31,8 @@ def insta_scrapper(username_scrape):
              "Bio": profile.biography
              }
         y = str(json.dumps(x))
-        with open(f"{username_scrape}/general_details.json", "a",encoding="utf-8") as writer:  # for writing
+        # with open(f"{username_scrape}/general_details.json", "a", encoding="utf-8") as writer:  # for writing
+        with open(username_scrape+"/general_details.json", "a", encoding="utf-8",) as writer:
             writer.write(y)
         logger.info("DONE")
 
@@ -41,7 +42,7 @@ def insta_scrapper(username_scrape):
         logger.info("Getting followers")
         for followee in profile.get_followers():
             follow_list.append(followee.username)
-            file = open(f"{username_scrape}/followers.csv", "a+")
+            file = open(username_scrape+"/followers.csv", "a+")
             file.write(follow_list[count])
             file.write("\n")
             file.close()
@@ -55,7 +56,7 @@ def insta_scrapper(username_scrape):
         logger.info("Getting followees")
         for followee in profile.get_followees():
             following_list.append(followee.username)
-            file = open(f"{username_scrape}/followingss.csv", "a+")
+            file = open(username_scrape+"/followingss.csv", "a+")
             file.write(following_list[count])
             file.write("\n")
             file.close()
@@ -77,7 +78,7 @@ def insta_scrapper(username_scrape):
             logger.info(f"Getting likes for {post_likes}")
             for like in post_likes:
                 list_likers.append((like.username))
-                likers_file = open(f"{username_scrape}/likes_post.csv", "a+")
+                likers_file = open(username_scrape+"/likes_post.csv", "a+")
                 likers_file.write(list_likers[n])
                 n += 1
                 # if count % 1000 == 0:
@@ -86,7 +87,7 @@ def insta_scrapper(username_scrape):
                 likers_file.close()
             x = {f"Count of post_{post_likes}": n}
             y = str(json.dumps(x))
-            count_comments = open(f"{username_scrape}/counts of likes in Post.json", "a+")
+            count_comments = open(username_scrape+"/counts of likes in Post.json", "a+")
             count_comments.write(y)
             count_comments.write("\n")
             count_comments.close()
@@ -95,7 +96,7 @@ def insta_scrapper(username_scrape):
             logger.info(f"Getting comments for {post_comments}")
             for comment in post_comments:
                 list_comment.append((comment.owner.username))
-                comment_file = open(f"{username_scrape}/comment_post.csv", "a+")
+                comment_file = open(username_scrape+"/comment_post.csv", "a+")
                 comment_file.write(list_comment[n])
                 # if count % 1000 == 0:
                 #     time.sleep(1000)
@@ -104,29 +105,29 @@ def insta_scrapper(username_scrape):
                 comment_file.close()
             x = {f"Count of post_{post_comments}": n}
             y = str(json.dumps(x))
-            count_comments = open(f"{username_scrape}/counts of comments in Post.json", "a+")
+            count_comments = open(username_scrape+"/counts of comments in Post.json", "a+")
             count_comments.write(y)
             count_comments.write("\n")
             count_comments.close()
         logger.info("Removing Duplicates from files")
-        df_likers = pd.read_csv(f"{username_scrape}/likes_post.csv", header=None)
+        df_likers = pd.read_csv(username_scrape+"/likes_post.csv", header=None)
         df_likers.rename(columns={0: "Username"}, inplace=True)
         df_likers = df_likers.drop_duplicates(subset = "Username")
-        df_likers.to_csv(f"{username_scrape}/likes_post.csv",index = False)
-        df_commnets = pd.read_csv(f"{username_scrape}/comment_post.csv", header = None)
+        df_likers.to_csv(username_scrape+"/likes_post.csv",index = False)
+        df_commnets = pd.read_csv(username_scrape+"/comment_post.csv", header = None)
         df_commnets.rename(columns={0:"Username"}, inplace=True)
         df_commnets = df_commnets.drop_duplicates(subset = "Username")
-        df_commnets.to_csv(f"{username_scrape}/comment_post.csv",index = False)
+        df_commnets.to_csv(username_scrape+"/comment_post.csv",index = False)
         logger.info("list of all the likers and commenters have been made\n")
     except Exception as e:
         logger.warning(e)
-        print(f"ERROR for {username_scrape}")
+        print(f"ERROR for", username_scrape)
 
 
 # function to collect username for scraping the data
 def scrape_followers(name):
     """Scrapes followers for a particular account"""
-    logger.info(f"Scraping {name}")
+    logger.info("Scraping", name)
     instance = instaloader.Instaloader(save_metadata=False, compress_json=False, )
     instance.login(user=os.environ['INSTA_USERNAME_2'], passwd=os.environ['INSTA_PASS_2'])
     profile = instaloader.Profile.from_username(instance.context, username=name)
@@ -153,9 +154,9 @@ if __name__ == '__main__':
     follower_list = list(follower_data["username"])
     count = 0
     for name in follower_list:
-        logger.info(f"Scrapping {name}")
+        logger.info("Scrapping", name)
         insta_scrapper(name)
-        logger.info(f"Scraped {name}")
+        logger.info("Scraped", name)
         follower_data = follower_data.drop([count])
         follower_data.to_csv("data/Instagram/data.csv",index=False)
         count+=1
